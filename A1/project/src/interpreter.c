b88c478b7b18c46aafe9eefada23a0c811c57a27
Shiyan <shiyan.wliu@gmail.com>
2026-01-26 14:21:42 -0500

    Up to ls

---
+++ A1/project/src/interpreter.c
@@ -0,0 +1,209 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include "shellmemory.h"
+#include "shell.h"
+
+int MAX_ARGS_SIZE = 3;
+int MAX_DIR_SIZE = 256;
+int MAX_FILENAME_SIZE = 128;
+int badcommand() {
+    printf("Unknown Command\n");
+    return 1;
+}
+
+// For source command only
+int badcommandFileDoesNotExist() {
+    printf("Bad command: File not found\n");
+    return 3;
+}
+
+int help();
+int quit();
+int set(char *var, char *value);
+int print(char *var);
+int source(char *script);
+int echo(char * string);
+int my_ls();
+int badcommandFileDoesNotExist();
+
+// Interpret commands and their arguments
+int interpreter(char *command_args[], int args_size) {
+    int i;
+
+    if (args_size < 1 || args_size > MAX_ARGS_SIZE) {
+        return badcommand();
+    }
+
+    for (i = 0; i < args_size; i++) {   // terminate args at newlines
+        command_args[i][strcspn(command_args[i], "\r\n")] = 0;
+    }
+
+    if (strcmp(command_args[0], "help") == 0) {
+        //help
+        if (args_size != 1)
+            return badcommand();
+        return help();
+
+    } else if (strcmp(command_args[0], "quit") == 0) {
+        //quit
+        if (args_size != 1)
+            return badcommand();
+        return quit();
+
+    } else if (strcmp(command_args[0], "set") == 0) {
+        //set
+        if (args_size != 3)
+            return badcommand();
+        return set(command_args[1], command_args[2]);
+
+    } else if (strcmp(command_args[0], "print") == 0) {
+        //print
+        if (args_size != 2)
+            return badcommand();
+        return print(command_args[1]);
+
+    } else if (strcmp(command_args[0], "source") == 0) {
+        //source
+        if (args_size != 2)
+            return badcommand();
+        return source(command_args[1]);
+
+    } else if (strcmp(command_args[0], "echo") == 0) {
+        //echo
+        if (args_size != 2)
+            return badcommand();
+        return echo(command_args[1]);
+    } else if (strcmp(command_args[0], "my_ls") == 0) {
+        //ls
+        if (args_size != 1)
+            return badcommand();
+        return my_ls();
+    }
+    
+    else
+        return badcommand();
+}
+
+int help() {
+
+    // note the literal tab characters here for alignment
+    char help_string[] = "COMMAND			DESCRIPTION\n \
+help			Displays all the commands\n \
+quit			Exits / terminates the shell with “Bye!”\n \
+set VAR STRING		Assigns a value to shell memory\n \
+print VAR		Displays the STRING assigned to VAR\n \
+source SCRIPT.TXT	Executes the file SCRIPT.TXT\n ";
+    printf("%s\n", help_string);
+    return 0;
+}
+
+int quit() {
+    printf("Bye!\n");
+    exit(0);
+}
+
+int set(char *var, char *value) {
+    // Challenge: allow setting VAR to the rest of the input line,
+    // possibly including spaces.
+
+    // Hint: Since "value" might contain multiple tokens, you'll need to loop
+    // through them, concatenate each token to the buffer, and handle spacing
+    // appropriately. Investigate how `strcat` works and how you can use it
+    // effectively here.
+
+    mem_set_value(var, value);
+    return 0;
+}
+
+
+int print(char *var) {
+    printf("%s\n", mem_get_value(var));
+    return 0;
+}
+
+int source(char *script) {
+    int errCode = 0;
+    char line[MAX_USER_INPUT];
+    FILE *p = fopen(script, "rt");      // the program is in a file
+
+    if (p == NULL) {
+        return badcommandFileDoesNotExist();
+    }
+
+    fgets(line, MAX_USER_INPUT - 1, p);
+    while (1) {
+        errCode = parseInput(line);     // which calls interpreter()
+        memset(line, 0, sizeof(line));
+
+        if (feof(p)) {
+            break;
+        }
+        fgets(line, MAX_USER_INPUT - 1, p);
+    }
+
+    fclose(p);
+
+    return errCode;
+}
+
+int echo(char *string){
+    if (string[0] == '$') {
+        char tmp[100] = "";
+
+        for (int i = 1; i < strlen(string); i++){
+            tmp[i-1] = string[i];
+        }
+        char* INVALID_STRING = "Variable does not exist";
+        char* mem_result = mem_get_value(tmp);
+
+        if (strcmp(mem_result, INVALID_STRING) == 0){
+            mem_result = "";
+        }
+        printf("%s", mem_result);
+        printf("\n");
+    }
+    else{
+        printf("%s", string);
+        printf("\n");
+    }
+    return 0;
+}
+
+//Helper function for string comparisons.
+int compare(const void *a, const void *b) {
+    const char *sa = (const char *)a;
+    const char *sb = (const char *)b;
+    return strcmp(sa, sb);
+}
+
+
+int my_ls(){
+    DIR *dirp;
+    struct dirent *entry;
+
+    char files[MAX_DIR_SIZE][MAX_FILENAME_SIZE];
+    dirp = opendir(".");
+
+    int i = 0;
+    while(1){
+        entry = readdir(dirp);
+
+        if (entry != NULL){
+            strcpy(files[i], entry->d_name);
+            i += 1;
+            continue;
+        }
+
+        break;
+    }
+
+    qsort(files, i, sizeof(files[0]), compare);
+    for (int j = 0; j < i; j++){
+        printf("%s\n", files[j]);
+    }
+
+    closedir(dirp);
+    return 0;
+}
